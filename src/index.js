22650d0d3f9acc04cff379834ac2d42b352a2362
lorypelli <lorypelli@proton.me>
2025-09-02 08:02:13 +0200

    don't use string concatenation

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const BASE_URL = 'https://lorypelli.dev/stats';
 
 export default {
     fetch: async (req) =>
-        await fetch(`${BASE_URL}${new URL(req.url).pathname}`, req),
+        await fetch(BASE_URL + new URL(req.url).pathname, req),
 };
