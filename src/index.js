00ec24cdb69abaae4924932ff45997771b9bf11a
lorypelli <lorypelli@proton.me>
2025-09-02 08:36:45 +0200

    type hint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const BASE_URL = 'https://lorypelli.dev/stats';
 
 export default {
-    fetch: async (req) =>
+    fetch: async (/** @type { Request } */ req) =>
         await fetch(BASE_URL + new URL(req.url).pathname, req),
 };
