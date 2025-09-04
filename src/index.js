f5669cc3ad273d7c5d2517b3a705ae3a4e07ccad
lorypelli <lorypelli@proton.me>
2025-09-04 10:55:37 +0200

    don't `await` the request

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const BASE_URL = 'https://lorypelli.dev/stats';
 
 export default {
-    fetch: async (/** @type { Request } */ req) =>
-        await fetch(BASE_URL + new URL(req.url).pathname, req),
+    /** @param { Request } req */
+    fetch: (req) => fetch(BASE_URL + new URL(req.url).pathname, req),
 };
